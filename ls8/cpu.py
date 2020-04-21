ebe15fc2611755a2d69df845a336a30d863c30f9
josemariodev <josemariodev@gmail.com>
2020-04-21 10:32:06 -0500

    :sparkles: step 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,31 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
+ # Step 1: Add the constructor to cpu.py
+    # Add list properties to the CPU class to hold 256 bytes of memory and 8 general-purpose registers.
+    # Also add properties for any internal registers you need, e.g. PC.
+    # Later on, you might do further initialization here, e.g. setting the initial value of the stack pointer.
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # 256 bytes of memory
+        self.ram = [0] * 256
+        # 8 general-purpose registers
+        self.register = [0] * 8
+        # PC
+        self.pc = 0
+        # Commands
+        self.commands = {
+            0b00000001: self.hlt,
+            0b10000010: self.ldi,
+            0b01000111: self.prn
+        }
+    # accepts the address in RAM and returns the value stored there
+
+    
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +37,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +66,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
