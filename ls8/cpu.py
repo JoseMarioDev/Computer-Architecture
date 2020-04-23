d7c8590ced288aa48c1e62d8c4aa225e6d101559
josemariodev <josemariodev@gmail.com>
2020-04-23 15:30:42 -0500

    :sparkles: step 10: Implement stack

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,77 +6,31 @@ import sys
 class CPU:
     """Main CPU class."""
 
-    # Step 1: Add the constructor to cpu.py
-    # Add list properties to the CPU class to hold 256 bytes of memory and 8 general-purpose registers.
-    # Also add properties for any internal registers you need, e.g. PC.
-    # Later on, you might do further initialization here, e.g. setting the initial value of the stack pointer.
-
+    # LS8 instruction definitions
     HLT = 1
-    LDI = 130
+    RET = 17
+    PUSH = 69
+    POP = 70
     PRN = 71
+    CALL = 80
+    LDI = 130
+    ADD = 160
     MUL = 162
 
     def __init__(self):
         """Construct a new CPU."""
-        # 256 bytes of memory
-        self.ram = [0] * 256
-        # 8 general-purpose registers
-        self.register = [0] * 8
-        # PC
         self.pc = 0
-
-    # accepts the address in RAM and returns the value stored there
-
-    # Step 2: Add RAM functions
-    # In CPU, add method ram_read() and ram_write() that access the RAM inside the CPU object.
-    # ram_read() should accept the address to read and return the value stored there.
-    # raw_write() should accept a value to write, and the address to write it to.
-
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, address, value):
-        self.ram[address] = value
-
-    # Step 4
-    # halt the CPU and exit the emulator
-    def hlt(self, operand_a, operand_b):
-        return (0, False)
-    # load immediate, store a value in a register, or set this register to this value
-
-    # Step 5: Add the LDI instruction
-
-    def ldi(self, operand_a, operand_b):
-        self.reg[operand_a] = operand_b
-        return (3, True)
-
-    # Step 6: Add the PRN instruction
-    # prints the numeric value stored in a register
-
-    def prn(self, operand_a, operand_b):
-        print(self.reg[operand_a])
-        return (2, True)
-
-    # def load(self):
-    #     """Load a program into memory."""
-
-    #     address = 0
-
-    #     # For now, we've just hardcoded a program:
-
-    #     program = [
-    #         # From print8.ls8
-    #         0b10000010,  # LDI R0,8
-    #         0b00000000,
-    #         0b00001000,
-    #         0b01000111,  # PRN R0
-    #         0b00000000,
-    #         0b00000001,  # HLT
-    #     ]
-
-    #     for instruction in program:
-    #         self.ram[address] = instruction
-    #         address += 1
+        self.ram = [00000000] * 256
+        self.reg = [0] * 8
+        self.reg[7] = 0xF4
+        self.dispatch = {}
+        self.dispatch[self.HLT] = self.halt
+        self.dispatch[self.LDI] = self.load_immediate
+        self.dispatch[self.PRN] = self.sys_out
+        self.dispatch[self.MUL] = self.mul
+        self.dispatch[self.ADD] = self.add
+        self.dispatch[self.PUSH] = self.push
+        self.dispatch[self.POP] = self.pop
 
     def load(self, filename):
         print(filename)
@@ -89,7 +43,7 @@ class CPU:
                 for line in f:
                     # Parse out comments
                     comment_split = line.strip().split("#")
-                    # Cast the numbers from strings to ints
+                    # Cast the numbers from strings to int
                     value = comment_split[0].strip()
                     # Ignore blank lines
                     if value == "":
@@ -112,9 +66,19 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, address):
+        if address > len(self.ram):
+            return 0
+        return self.ram[address]
+
+    def ram_write(self, address, data):
+        self.ram[address] = data
 
     def trace(self):
         """
@@ -132,33 +96,60 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.register[i], end='')
+            print(" %02X" % self.reg[i], end='')
 
         print()
 
+    def halt(self):
+        exit()
+
+    def load_immediate(self):
+        target_register = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+        self.reg[target_register] = value
+        self.pc = self.pc + 3
+
+    def sys_out(self):
+        print(self.reg[self.ram_read(self.pc + 1)])
+        self.pc = self.pc + 2
+
+    def add(self):
+        self.alu('ADD', self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
+        self.pc = self.pc + 3
+
+    def mul(self):
+        self.alu('MUL', self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
+        self.pc = self.pc + 3
+
+    def push(self):
+        self.reg[7] = self.reg[7] - 1
+        self.ram_write(self.reg[7], self.reg[self.ram_read(self.pc + 1)])
+        self.pc = self.pc + 2
+
+    def pop(self):
+        self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.reg[7])
+        self.reg[7] = self.reg[7] + 1
+        self.pc = self.pc + 2
+
     def run(self):
-        # Step 3: Implement the core of CPU's run() method
         """Run the CPU."""
+
         running = True
 
         while running:
             ir = self.ram_read(self.pc)
 
-            if ir == self.HLT:
-                running = False
-            elif ir == self.LDI:
-                target_register = self.ram_read(self.pc + 1)
-                value = self.ram_read(self.pc + 2)
-                self.register[target_register] = value
-                self.pc = self.pc + 3
-            elif ir == self.PRN:
-                print(self.register[self.ram_read(self.pc + 1)])
-                self.pc = self.pc + 2
-            elif ir == self.MUL:
-                print(self.register[self.ram_read(self.pc + 1)]
-                      * self.register[self.ram_read(self.pc + 2)])
-                self.pc = self.pc + 3
-            else:
+            # We track the counter to make sure the
+            # instruction we ran moved the counter,
+            # if it didn't, we advance to next instruction,
+            old_counter = self.pc
+
+            if ir not in self.dispatch:
                 print('Bad instruction')
                 self.trace()
-                running = False
+                self.halt()
+
+            self.dispatch[ir]()
+
+            if self.pc == old_counter:
+                self.pc = self.pc + 1
